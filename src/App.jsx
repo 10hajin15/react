586885a90fed9233dca4afea72fa0d71fb8418c7
10hajin15 <10hajin15@gmail.com>
2024-05-22 16:45:02 +0900

    Dialog 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,13 @@
 import React from "react";
-import { getComponentName } from "./shared/lib/utils";
+import MainPage from "./pages/MainPage";
+import * as MyLayout from "./MyLayout"
 
 const App = () => {
   return (
-    <>
-      <EnhancedHeader />
-      <EnhancedButton />
-    </>
+    <MyLayout.Layout>
+      <MainPage />
+    </MyLayout.Layout>
   );
 };
 
 export default App;
-
-// [고차 컴포넌트]
-// 컴포넌트가 마운트 될 때 기록한다
-// 버튼 컴포넌트를 클릭할 때 기록한다
-// class Header extends React.Component {
-//   componentDidMount() {
-//     console.log("[Header] 마운트");
-//   }
-
-//   render() {
-//     return <header>Header</header>;
-//   }
-// }
-
-// class Button extends React.Component {
-//   componentDidMount() {
-//     console.log("[Button] 마운트");
-//   }
-
-//   handleClick = () => {
-//     console.log("[Button] 클릭");
-//   };
-
-//   render() {
-//     return <button onClick={this.handleClick}>버튼</button>;
-//   }
-// }
-
-
-
-const withLogging = (WrappedComponent) => {
-  function log(message) {
-    console.log(`[${getComponentName(WrappedComponent)}] ${message}`);
-  }
-
-  class WithLogging extends React.Component {
-    render() {
-      const enhancedProps = { log };
-      return <WrappedComponent {...this.props} {...enhancedProps} />;
-    }
-
-    componentDidMount() {
-      log("마운트");
-    }
-  }
-
-  return WithLogging;
-};
-
-const Header = () => <header>헤더</header>
-const Button = ({log}) => {
-  const handleClick = () => log('클릭')
-  return <button onClick={handleClick}>버튼</button>
-}
-
-const EnhancedHeader = withLogging(Header)
-const EnhancedButton = withLogging(Button)
---
+++ src/components/PaymentSuccessDialog.jsx
@@ -0,0 +1,24 @@
+import Dialog from "./Dialog"
+import * as MyLayout from "../MyLayout"
+
+const PaymentSuccessDialog = ({closeDialog}) => {
+  const handleClick = () => {
+    closeDialog()
+  }
+
+  return (
+    <Dialog
+      header={<>결제 완료</>}
+      footer={
+      <>
+        <button onClick={handleClick}>아니오</button>
+        <button onClick={handleClick}>네</button>
+      </>
+      }
+    >
+      결제가 완료되었습니다.
+    </Dialog>
+  )
+}
+
+export default MyLayout.withLayout(PaymentSuccessDialog)
