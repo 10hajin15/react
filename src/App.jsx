2e843c453ec4a68bd317fb5d80d9128be4cd4477
10hajin15 <10hajin15@gmail.com>
2024-05-23 15:53:46 +0900

    useEffect 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,59 @@
 import React from "react";
 import MyReact from "./lib/MyReact";
 
-function NameField() {
-  const [firstname, setFirstname] = MyReact.useState("사용자1");
-  const [lastname, setLastname] = MyReact.useState("김");
+const Counter = () => {
+  MyReact.resetCursor();
+  const [count, setCount] = React.useState(0);
+  const [name, setName] = React.useState("");
 
-  const handleChangeFirstname = (e) => setFirstname(e.target.value);
-  const handleChangeLastname = (e) => setLastname(e.target.value);
+  const handleClick = () => setCount(count + 1);
+  const handleChangeName = (e) => setName(e.target.value);
+
+  MyReact.useEffect(() => {
+    setName(localStorage.getItem("name") || "");
+    console.log("effect3");
+  }, []);
+
+  MyReact.useEffect(() => {
+    document.title = `count: ${count} | name: ${name}`;
+    console.log("effect1");
+
+    return function cleanup() {
+      document.title = "";
+      console.log("effect1 cleanup");
+    };
+  }, [count, name]);
+
+  MyReact.useEffect(() => {
+    localStorage.setItem("name", name);
+    console.log("effect2");
+  }, [name]);
+
+  console.log("Counter rendered");
+  return (
+    <>
+      <button onClick={handleClick}>더하기</button>
+      <input value={name} onChange={handleChangeName} />
+    </>
+  );
+};
+
+// export default () => <Counter />;
+
+const Wrapper = () => {
+  const [mounted, setMounted] = React.useState(false);
+  const handleToggle = () => {
+    const nextMounted = !mounted;
+    if (!nextMounted) MyReact.cleanupEffects();
+    setMounted(nextMounted);
+  };
 
   return (
     <>
-      <input value={firstname} onChange={handleChangeFirstname} />
-      <input value={lastname} onChange={handleChangeLastname} />
+      <button onClick={handleToggle}>컴포넌트 토글</button>
+      {mounted && <Counter />}
     </>
   );
-}
+};
 
-export default () => <NameField />;
+export default () => <Wrapper />;
--- src/lib/MyReact.jsx
+++ src/lib/MyReact.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 const MyReact = (function MyReact() {
-  const memorizedState = []
+  const memorizedState = [];
+  const deps = [];
   const isInitialized = [];
+  const cleanups = [];
   let cursor = 0;
 
   function useState(initialValue = "") {
@@ -13,15 +15,15 @@ const MyReact = (function MyReact() {
       isInitialized[cursor] = true;
     }
 
-    const state = memorizedState[cursor]
+    const state = memorizedState[cursor];
 
-    const setStateAt = _cursor => nextState => {
+    const setStateAt = (_cursor) => (nextState) => {
       if (state === nextState) return;
       memorizedState[_cursor] = nextState;
       forceUpdate();
     };
-    const setState = setStateAt(cursor)
-    cursor += 1
+    const setState = setStateAt(cursor);
+    cursor += 1;
 
     return [state, setState];
   }
@@ -30,15 +32,54 @@ const MyReact = (function MyReact() {
   function useForceUpdate() {
     const [value, setValue] = React.useState(1);
     const forceUpdate = () => {
-      setValue(value + 1)
-      cursor = 0
+      setValue(value + 1);
+      cursor = 0;
     };
     return {
       forceUpdate,
     };
   }
 
-  return { useState };
+  function useEffect(effect, nextDeps) {
+    function runDeferredEffect() {
+      function runEffect() {
+        const cleanup = effect();
+        if (cleanup) cleanups[cursor] = cleanup;
+      }
+      const ENOUGH_TIME_TO_RENDER = 1;
+      setTimeout(runEffect, ENOUGH_TIME_TO_RENDER);
+    }
+
+    if (!isInitialized[cursor]) {
+      isInitialized[cursor] = true;
+      deps[cursor] = nextDeps;
+      cursor = cursor + 1;
+      runDeferredEffect();
+      return;
+    }
+
+    const prevDeps = deps[cursor];
+    const depsSame = prevDeps.every(
+      (prevDep, index) => prevDep === nextDeps[index]
+    );
+    if (depsSame) {
+      cursor = cursor + 1;
+      return;
+    }
+    deps[cursor] = nextDeps;
+    cursor = cursor + 1;
+    runDeferredEffect();
+  }
+
+  function resetCursor() {
+    cursor = 0;
+  }
+
+  function cleanupEffects() {
+    cleanups.forEach((cleanup) => typeof cleanup === "function" && cleanup());
+  }
+
+  return { useState, useEffect, resetCursor, cleanupEffects };
 })();
 
 export default MyReact;
